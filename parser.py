66ca2acc9b01137a05eaac578f50986aeb27db59
Yaroshchak.S.V <yaroschak.s.v@gmail.com>
2020-05-13 14:32:27 +0300

    add phone

--- parser.py
+++ parser.py
@@ -1,26 +1,40 @@
 import requests
 import lxml.html
+import re
+import sqlite3
 
-base_url = 'https://www.work.ua'
+# base_url = 'https://www.work.ua'
 
 
-# with requests.session() as s:
-#     rez = s.get('https://www.work.ua/jobs/2991081')
-#     rez = s.post('https://www.work.ua/jobs/2991081/ajax/get-jobs-data/',data={},headers={'referer':'https://www.work.ua/jobs/2991081'})
-#     print(rez.headers)
-#     print(rez.json())
 
-# link = 'https://www.work.ua/jobs/2991081/ajax/get-jobs-data/'
-# link = 'https://www.work.ua/ajax/get-jobs-data/'
-# ajax/get-jobs-data/
-# rez = requests.post(link,data={})
-# print(rez.status_code)
+
+# rez = requests.post('https://www.work.ua/jobs/2991081/ajax/get-jobs-data/',data={},headers={'User-Agent': user_agents[0]})
+# print(rez.json())
+
+
 
 
 
 class WorkUa:
     def __init__(self,base_url):
         self.base_url = base_url
+        self.user_agents = ["Google Chrome 53 (Win 10 x64): Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36", 
+                           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36"]
+        self.conn = sqlite3.connect('info_work.db')
+        self.cur =  self.conn.cursor()
+        self.cur.execute("""CREATE TABLE IF not EXISTS work
+                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    link text,
+                    title text, 
+                    sel text,
+                    kompani text,
+                    time text,
+                    phone text
+                    )"""
+
+
+            )
+
 
 
     def get_info(self):
@@ -30,7 +44,7 @@ class WorkUa:
         cards = []
         while flag:
             url = '{0}/jobs-rivne-it/?advs=1&page={1}'.format(self.base_url,page)
-            rez = requests.get(url)
+            rez = requests.get(url,headers={'User-Agent': self.user_agents[0]})
             if rez.status_code==200:
                 dom = lxml.html.fromstring(rez.text)
                 divs = dom.xpath("//div[@class='card card-hover card-visited wordwrap job-link']")
@@ -49,7 +63,7 @@ class WorkUa:
 
                         cards.append(
                                 {
-                                    'link':base_url+a.attrib.get('href'),
+                                    'link':self.base_url+a.attrib.get('href'),
                                     'title':a.text_content(),
                                     'sel':div_b.text_content(),
                                     'kompani':kompani.text_content(),
@@ -60,10 +74,47 @@ class WorkUa:
                 print('Error', rez.status_code)
 
             page = page + 1
+        
+# link text,
+#                     title text, 
+#                     sel text,
+#                     kompani text,
+#                     time text,
+#                     phone text
+        sql_insert = """
+        INSERT INTO work (link,title,sel,kompani,time,phone) VALUES(?,?,?,?,?,?) 
+        """
+        for card in cards:
+            rez = requests.get(card['link'],headers={'User-Agent': self.user_agents[0]})
+            if rez.status_code==200:
+                dom = lxml.html.fromstring(rez.text)
+                tel = dom.xpath("//span[@id='contact-phone']")
+                if tel:
+                    tel_info = requests.post('{0}ajax/get-jobs-data/'.format(card['link']),data={},headers={'User-Agent': self.user_agents[0]})
+                    if tel_info.status_code == 200:
+                        contactPhone = tel_info.json().get('contactPhone')
+                        contactPhone = re.sub(r'[-\(\)+]','',contactPhone)
+                        contactPhone = re.findall(r'tel:(\d+)',contactPhone)
+                        card['phone'] = contactPhone[0]
+
+            
+            self.cur.execute(sql_insert,(card['link'],card['title'],card['sel'],card['kompani'],card.get('time'),card.get('phone')))
+        
+        self.conn.commit()
+        self.conn.close()
+
+
+
 
         return cards
 
 
 
 work = WorkUa('https://www.work.ua')
-print(work.get_info())
+for info in work.get_info():
+    print(info)
+    print()
+
+
+
+
